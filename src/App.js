0d7dc96c0432a1f8254469a64e9d0d94836cf5d9
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-01 12:00:31 -0600

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import IconWebsite from "./components/iconwebsite";
 import IcontTwitter from "./components/icontwitter";
 import IconSun from "./components/iconsun";
 import IconCompany from "./components/iconcompany";
+import Notfound from "./assets/not found.png";
 
 import { githubApiUrl } from "./helper";
 import IconTwitter from "./components/icontwitter";
@@ -36,14 +37,20 @@ function App() {
       const response = await fetch(githubApiUrl(username));
       const githubUser = await response.json();
       const login = githubUser?.login;
+      console.log(login);
       if (login) {
         // then we do have a user
         setUser(githubUser);
+        setError(null);
         return;
       }
       // we do not have a user
       setError("No result.");
-    } catch (err) {}
+      setUser(null);
+    } catch (err) {
+      setError("No result.");
+      setUser(null);
+    }
   };
   return (
     <div className={`github-app ${darkMode ? "dark" : ""}`}>
@@ -68,61 +75,83 @@ function App() {
               value={username}
               onChange={(event) => setUsername(event.target.value)}
             />
+            {error && (
+              <b
+                style={{
+                  color: "tomato",
+                  marginRight: "6px",
+                  fontSize: ".8em",
+                }}
+              >
+                {error}
+              </b>
+            )}
             <button className="btn" onClick={searchUser}>
               Search
             </button>
           </div>
-          <section className="search-content">
-            {/* <aside className="user-container"></aside> */}
-            <div className="img-head-container">
-              <div className="img-container">
-                <img src={avatar_url} alt="Github user profile" />
-              </div>
-              <header className="head">
-                <div className="head-left">
-                  <h3>{name}</h3>
-                  <p className="username">@{login}</p>
-                </div>
-
-                <p>Joined at Mar 14</p>
-              </header>
+          {!user ? (
+            <div
+              className="not-found"
+              style={{ padding: "4em 0", textAlign: "center" }}
+            >
+              <img src={Notfound} alt="No where to be seen" />
+              <h1>Not found or No Search yet.</h1>
+              <h3> Try searchin..</h3>
             </div>
-            <section className="user-details">
-              <p className="bio">{bio}</p>
-              <div className="repo-details">
-                <div className="r-details">
-                  <p>Repos</p>
-                  <b>{public_repos}</b>
+          ) : (
+            <section className="search-content">
+              {/* <aside className="user-container"></aside> */}
+              <div className="img-head-container">
+                <div className="img-container">
+                  <img src={avatar_url} alt="Github user profile" />
                 </div>
-                <div className="r-details">
-                  <p>Followers</p>
-                  <b>{followers}</b>
-                </div>
-                <div className="r-details">
-                  <p>Following</p>
-                  <b>{following}</b>
-                </div>
+                <header className="head">
+                  <div className="head-left">
+                    <h3>{name}</h3>
+                    <p className="username">@{login}</p>
+                  </div>
+
+                  <p>Joined at Mar 14</p>
+                </header>
               </div>
-              <div className="socials">
-                <span className="social">
-                  <IconLocation />
-                  {location ? location : "Not available"}
-                </span>
-                <span className="social">
-                  <IconWebsite />
-                  {blog ? <a href={blog}>{blog}</a> : "Not available"}
-                </span>
-                <span className="social">
-                  <IconTwitter />
-                  {twitter_username ? "Twitter" : "Not available"}
-                </span>
-                <span className="social">
-                  <IconCompany />
-                  {company ? company : "Not available"}
-                </span>
-              </div>
+              <section className="user-details">
+                <p className="bio">{bio}</p>
+                <div className="repo-details">
+                  <div className="r-details">
+                    <p>Repos</p>
+                    <b>{public_repos}</b>
+                  </div>
+                  <div className="r-details">
+                    <p>Followers</p>
+                    <b>{followers}</b>
+                  </div>
+                  <div className="r-details">
+                    <p>Following</p>
+                    <b>{following}</b>
+                  </div>
+                </div>
+                <div className="socials">
+                  <span className="social">
+                    <IconLocation />
+                    {location ? location : "Not available"}
+                  </span>
+                  <span className="social">
+                    <IconWebsite />
+                    {blog ? <a href={blog}>{blog}</a> : "Not available"}
+                  </span>
+                  <span className="social">
+                    <IconTwitter />
+                    {twitter_username ? "Twitter" : "Not available"}
+                  </span>
+                  <span className="social">
+                    <IconCompany />
+                    {company ? company : "Not available"}
+                  </span>
+                </div>
+              </section>
             </section>
-          </section>
+          )}
         </main>
       </div>
     </div>
