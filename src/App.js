c0090f743ea2e7f89a0ad08b09252be1266201e5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-01 09:13:53 -0600

    working on structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import { useState } from "react";
 import IconSearch from "./components/iconsearch";
+import IconMoon from "./components/iconmoon";
+import { githubApiUrl } from "./helper";
 
 function App() {
   const [username, setUsername] = useState("");
+
+  const searchUser = async () => {
+    if (username.trim() === "") return;
+    try {
+      const response = await fetch(githubApiUrl(username));
+      const githubUser = await response.json();
+
+      console.log(githubUser);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="github-app">
-      <nav className="nav"></nav>
+      <nav className="nav">
+        <h2>Devfinder</h2>
+
+        <div className="toggle-theme">
+          <span>Dark</span>
+          <IconMoon />
+        </div>
+      </nav>
       <main className="main-container">
         <div className="search-container">
           <IconSearch />
@@ -15,9 +36,14 @@ function App() {
             value={username}
             onChange={(event) => setUsername(event.target.value)}
           />
-          <button className="btn">Search</button>
+          <button className="btn" onClick={searchUser}>
+            Search
+          </button>
         </div>
-        <section className="search-content"></section>
+        <section className="search-content">
+          <aside className="user-container"></aside>
+          <aside className="user-details"></aside>
+        </section>
       </main>
     </div>
   );
