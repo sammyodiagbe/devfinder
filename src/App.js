838036a68d4701fa7429043efb5f11385a1f3f64
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-28 18:02:53 -0600

    app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import IconSearch from "./components/iconsearch";
+
 function App() {
+  const [username, setUsername] = useState("");
   return (
     <div className="github-app">
       <nav className="nav"></nav>
       <main className="main-container">
-        <div className="search-container"></div>
+        <div className="search-container">
+          <IconSearch />
+          <input
+            type="text"
+            placeholder="@username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <button className="btn">Search</button>
+        </div>
         <section className="search-content"></section>
       </main>
     </div>
